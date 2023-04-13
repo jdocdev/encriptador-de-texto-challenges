c1008e8f00d5e6842115e0530dec81c6b1b267e2
juandavid_dev <juandavid.dev@gmail.com>
2023-04-13 18:02:50 -0500

    Desencriptado v.1.2

--- js/app.js
+++ js/app.js
@@ -10,14 +10,14 @@ const h3Encrypt = document.querySelector(".text-encrypt h3");
 // Selección del boton de copiar texto
 const copyButton = document.querySelector("#copyButton");
 
-copyButton.addEventListener("click", function() {
+copyButton.addEventListener("click", function () {
     const textEncrypt = document.querySelector("#textEncrypt").textContent;
-    navigator.clipboard.writeText(textEncrypt).then(function() {
-      console.log("Texto copiado al portapapeles");
-    }, function() {
-      console.error("No se pudo copiar el texto al portapapeles");
+    navigator.clipboard.writeText(textEncrypt).then(function () {
+        console.log("Texto copiado al portapapeles");
+    }, function () {
+        console.error("No se pudo copiar el texto al portapapeles");
     });
-  });
+});
 
 // borrar texto del textarea al recargar
 window.onload = function () {
@@ -43,7 +43,7 @@ function cifradoCesar(texto, desplazamiento) {
     return resultado;
 }
 
-// Funcion para copiar el texto al apretar el botón Encriptar
+// Función para encriptar el texto
 function encriptarCampo() {
 
     // Eliminar la imagen
@@ -52,10 +52,41 @@ function encriptarCampo() {
     h3Encrypt.style.display = 'none';
     // Mostrar botón de copiar
     copyButton.style.display = 'block';
-      
+
     const textBase = document.querySelector("#textBase").value;
     const desplazamiento = 19; // número de posiciones que se van a desplazar las letras en el alfabeto
     const textoCifrado = cifradoCesar(textBase, desplazamiento);
     document.querySelector("#textEncrypt").textContent = textoCifrado;
-  }
+}
+
+// Función de descifrado César
+function descifradoCesar(textoCifrado, desplazamiento) {
+    let resultado = "";
+    for (let i = 0; i < textoCifrado.length; i++) {
+        let caracter = textoCifrado[i];
+        let codigo = textoCifrado.charCodeAt(i);
+        if (codigo >= 65 && codigo <= 90) {
+            caracter = String.fromCharCode(((codigo - 65 - desplazamiento + 26) % 26) + 65);
+        } else if (codigo >= 97 && codigo <= 122) {
+            caracter = String.fromCharCode(((codigo - 97 - desplazamiento + 26) % 26) + 97);
+        }
+        resultado += caracter;
+    }
+    return resultado;
+}
+
+// Función para desencriptar el texto
+function desencriptarCampo() {
+    // Eliminar la imagen
+    imgEncrypt.style.display = 'none';
+    // Eliminar el titulo
+    h3Encrypt.style.display = 'none';
+    // Mostrar botón de copiar
+    copyButton.style.display = 'block';
+
+    const textBase = document.querySelector("#textEncrypt").textContent;
+    const desplazamiento = 19; // número de posiciones que se van a desplazar las letras en el alfabeto
+    const textoDesCifrado = descifradoCesar(textBase, desplazamiento);
+    document.querySelector("#textEncrypt").textContent = textoDesCifrado;
+}
 
